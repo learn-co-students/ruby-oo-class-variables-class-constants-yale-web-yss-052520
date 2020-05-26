bd09981b9a0be5d738f290c2b8903fbd3e14f86e
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-26 17:45:51 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,21 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
-    @brand = brand
+    self.brand = brand #self keyword to call brand= on this instance of Shoe
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  def brand=(new_brand)
+    @brand = new_brand
+    # Want to make sure BRANDS only contains unique values
+    BRANDS.include?(new_brand) ? BRANDS : BRANDS << new_brand
+  end   
 
 end
